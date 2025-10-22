9e71d0bdb24108ad97f187d3e639f9c79753b232
Ridwanul Basit <basitridwanul@gmail.com>
2025-10-22 14:17:42 +0600

    Fronend

---
+++ app/blogs/[id]/page.jsx
@@ -0,0 +1,72 @@
+'use client'
+import { assets, blog_data } from '@/Assets/assets';
+import Footer from '@/components/Footer';
+import Image from 'next/image';
+import Link from 'next/link';
+import React, { useEffect, useState } from 'react'
+
+const page = ({params}) => {
+
+   const [data,setdata] = useState(null);
+   const fetchBlogData = () =>{
+    for(let i=0; i<blog_data.length;i++){
+        if (Number(params.id)===blog_data[i].id){
+            setdata(blog_data[i]);
+            break;
+        }
+    }
+   }
+   useEffect(()=>{
+    fetchBlogData();
+
+   },[])
+
+  return (data? <>
+    <div className='bg-gray-200 py-5 px-5 md:px-12 lg:px-28'>
+        <div className='flex justify-between items-center'>
+            <Link href='/'> 
+            <Image src={assets.logo} width={180} alt='' className='w-[130px sm:w-auto' /> </Link>
+            <button className='flex items-center gap-2 font-medium py-1 px-3 sm:py-3 sm:px-6 border border-black shadow-[-7px_7px_0px_#000000]'>Get Started <Image
+                src={assets.arrow} alt='' /> </button> 
+        </div>
+        <div className='text-center my-24'>
+                <h1 className='text-2xl sm:text-5xl font-semibold max-w-[700px] mx-auto '>
+                    {data.title}
+                </h1>
+                <Image className='mx-auto mt-6 border border-white rounded-full' src={data.author_img} alt='' width={60} height={60}  />
+                <p className='mt-1 pb-2 text-lg max-width[740px] mx-auto '>{data.author}</p>
+            </div> 
+    </div>
+     <div className='mx-5 max-w-[800px] md:mx-auto mt-[-100px] mb-10'>
+            <Image className='border-4 border-white' src={data.image} width={1280} height={720} alt=''/>
+            <h1 className='my-8 text-[26px] font-semibold'>Introduction: </h1>
+            <p>{data.description}</p>
+            <h3 className='my-5 text-[18px] font-semibold'>Step 1: Self Reflection and Goal Seetings</h3>
+            <p className='my-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro earum impedit reprehenderit quisquam at, iure, dolorem veniam, laudantium vitae dolor harum ratione tenetur quos nesciunt facere corporis. Iusto, necessitatibus officiis.</p>
+            <p className='my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nihil! Aperiam eum aspernatur voluptates, pariatur a mollitia dolore neque veritatis non enim deleniti alias suscipit possimus nobis excepturi, quam repudiandae.</p>
+             <h3 className='my-5 text-[18px] font-semibold'>Step 2: Self Reflection and Goal Seetings</h3>
+            <p className='my-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro earum impedit reprehenderit quisquam at, iure, dolorem veniam, laudantium vitae dolor harum ratione tenetur quos nesciunt facere corporis. Iusto, necessitatibus officiis.</p>
+            <p className='my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nihil! Aperiam eum aspernatur voluptates, pariatur a mollitia dolore neque veritatis non enim deleniti alias suscipit possimus nobis excepturi, quam repudiandae.</p>
+             <h3 className='my-5 text-[18px] font-semibold'>Step 3: Self Reflection and Goal Seetings</h3>
+            <p className='my-3'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro earum impedit reprehenderit quisquam at, iure, dolorem veniam, laudantium vitae dolor harum ratione tenetur quos nesciunt facere corporis. Iusto, necessitatibus officiis.</p>
+            <p className='my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eveniet, nihil! Aperiam eum aspernatur voluptates, pariatur a mollitia dolore neque veritatis non enim deleniti alias suscipit possimus nobis excepturi, quam repudiandae.</p>
+             <h3 className='my-5 text-[18px] font-semibold'>Conclusion</h3>
+            <p className='my-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus repudiandae magnam, perspiciatis aliquid rerum, doloribus, pariatur nobis ratione sint eaque placeat molestias voluptatibus maxime neque quod numquam similique. Accusantium, tempora?</p>
+
+            <div className='my-24'>
+                <p className='text-black font font-semibold my-4'>Share this article on social media</p>
+                <div className='flex'>
+                   <Image src={assets.facebook_icon} alt='' width={50}/>
+                                <Image src={assets.twitter_icon} alt='' width={50}/>
+                                 <Image src={assets.googleplus_icon} alt='' width={50}/>
+                </div>
+            </div>
+            
+        </div>
+
+       <Footer></Footer>
+    </> : <></>
+  )
+}
+
+export default page
